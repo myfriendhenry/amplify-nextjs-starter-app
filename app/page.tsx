e806342ebd14f42bde34da223d71d4939c416449
myfriendhenry <46651049+myfriendhenry@users.noreply.github.com>
2024-01-05 07:04:41 -0700

    Update page.tsx pure ts?

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+
     
-//"use strict";
+"use strict";
 var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
         if (ar || !(i in from)) {
